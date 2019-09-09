f45caf92b4e4b54223a2d22b230d78f5d9c12219
lin-hung <kdalindahung@gmail.com>
2019-09-09 12:37:19 -0700

    added react-bootstrap and layout template

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import { Navbar, Nav, Form, FormControl, Button, Container, Jumbotron, Row, Col } from 'react-bootstrap'
 import Test from './Test'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Test/>
-        <p>
-          Text
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" fixed="top">
+        <Navbar.Brand href="#home">
+          <img src={logo} height="30px" alt="logo" />
+          Navbar
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+
+      <div id="content">
+        <Jumbotron>
+          <Container>
+            <Test />
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+          </Container>  
+        </Jumbotron>
+        <Container>
+          <Row>
+            <Col md={6}>
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </Col>
+            <Col>
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </Col>
+            <Col>
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </Col>
+          </Row>
+
+          <hr />
+
+        </Container>
+      </div>
+      <footer><Container>
+        <p>© Company 2017-2019</p>
+      </Container>
+      </footer>
     </div>
+
+
   );
 }
 
--- client/src/Test.js
+++ client/src/Test.js
@@ -1,31 +1,32 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
 
-class Test extends Component{
-    constructor(props){
+class Test extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            data:"data not loaded"
+        this.state = {
+            data: "data not loaded"
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getData()
     }
 
-    getData=()=>{
+    getData = () => {
         axios.get(`/test`)
-            .then(res=>{
+            .then(res => {
                 return res.data
             })
-            .then(data=>{
-                this.setState({data:data.abc})
+            .then(data => {
+                this.setState({ data: data.abc })
             })
     }
-   
-    render(){
-        return(
+
+    render() {
+        return (
             <div>
-                Data: {this.state.data}
+                <h1 className="display-3">Data: {this.state.data}
+                </h1>
             </div>
         )
     }
