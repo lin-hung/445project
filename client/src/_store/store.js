0815108ffa114c007b95915d316e60ed7fd31281
tiffany-lam <tiffclam@gmail.com>
2019-10-13 16:38:28 -0700

    test

--- client/src/_store/store.js
+++ client/src/_store/store.js
@@ -1,12 +1,19 @@
+<<<<<<< HEAD
 import { createStore, compose } from "redux";
+=======
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+>>>>>>> parent of 4de6980... removed redux-thunk
 import rootReducer from "../_reducers/rootreducer";
 
 const initialState = {};
+const middleware = [thunk];
 const store = createStore(
   rootReducer,
   //() => [],
   initialState,
   compose(
+    applyMiddleware(...middleware),
     ((window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose)
   ) 
 );
