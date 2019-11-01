d54c1cbbd5d451e4c4fda09ceaddffd1daf75051
Smushma <rollingrains@outlook.com>
2019-11-01 14:18:57 -0700

    Footer.js added.

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <Container>
+                    <p>© Company 2017-2019</p>
+                </Container>
+            </footer>
+        )
+    }
+}
