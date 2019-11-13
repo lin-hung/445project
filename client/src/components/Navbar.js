3ff7cf589b553bf9572681339afc6312fcc98775
Smushma <rollingrains@outlook.com>
2019-11-13 11:52:19 -0800

    Simple button changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,14 +9,22 @@ import { logoutAction } from '../_actions/authActions'
 class Navbar extends Component {
   loginButton = () => {
     if(this.props.auth.isAuthed) {
+<<<<<<< Updated upstream
       return(<Button onClick={this.props.logoutAction}>Log Out</Button>
+=======
+      return(<Button className="btn employeetButton" onClick={this.props.logoutAction}>Log Out</Button>
+>>>>>>> Stashed changes
       )
     }
     const pathname=window.location.pathname
     // if(pathname==='/' || pathname ==='/login' || pathname === '/register'){//if not logged in and on landing, login, register, don't show login button
     //   return(null)
     // }
+<<<<<<< Updated upstream
     return(<LinkContainer to='/login'><Button>Log In</Button></LinkContainer>)
+=======
+    return(<LinkContainer to='/login'><Button className="btn employeetButton">Log In</Button></LinkContainer>)
+>>>>>>> Stashed changes
   }
 
   render() {
@@ -35,4 +43,5 @@ class Navbar extends Component {
     )
   }
 }
+
 export default connect(mapAuthStateToProps, { logoutAction })(Navbar)
