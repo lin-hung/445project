f7925d6300c0fb4aa52194c7f47c27f9b43407ff
Ky-Vu <54649640+Ky-Vu@users.noreply.github.com>
2019-11-15 13:22:31 -0800

    Small change

--- client/src/components/CompanyProfile.js
+++ client/src/components/CompanyProfile.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './coprofileStyle.scss'
 import {TAGS} from './tags';
-import { render } from 'react-dom';
 import './tagStyle.scss'
 import { WithContext as ReactTags } from 'react-tag-input';
 const suggestions = TAGS.map((type) => {
