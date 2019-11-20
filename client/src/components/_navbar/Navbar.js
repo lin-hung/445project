6d7255a5511eb4ef40225624fbb72a7844a79b65
Ky-Vu <54649640+Ky-Vu@users.noreply.github.com>
2019-11-20 11:08:18 -0800

    Updated Navbar to link to appropriate profile pages

--- client/src/components/_navbar/Navbar.js
+++ client/src/components/_navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar as BSNav, Nav, Button } from 'react-bootstrap'
+import Axios from 'axios'
 import logo from '../../resources/logo.svg'
 import { mapAuthStateToProps } from '../../resources/utils'
 import { logoutAction } from '../../_actions/authActions'
@@ -11,66 +12,79 @@ import LoginModal from '../_auth/LoginModal'
 
 
 class Navbar extends Component {
+  state = {
+    profile: {}
+  }
   loginButton = () => {
     // const pathname=window.location.pathname
-    if(!this.props.auth.isAuthed){
-      return(
+    if (!this.props.auth.isAuthed) {
+      return (
         <LoginModal socket={this.props.socket} />
       )
-    } else if(this.props.auth.isAuthed) {
-      return(<Button id = "navButton" className="btn btn-primary employeetButton" href = "/" onClick={this.props.logoutAction}>Log Out</Button>
+    } else if (this.props.auth.isAuthed) {
+      return (<Button id="navButton" className="btn btn-primary employeetButton" href="/" onClick={this.props.logoutAction}>Log Out</Button>
       )
     }
 
+  }
+
+  componentDidMount() {
+    Axios.get('/api/profile/get').then((res) => {
+      console.log(res)
+      this.setState({ profile: res.data })
+    })
   }
 
   utilities = () => {
-    const pathname=window.location.pathname
-    
-    if(pathname==='/' || pathname ==='/login' || pathname === '/register'){//if not logged in and on landing, login, register, don't show login button
-      return(null)
+    const pathname = window.location.pathname
+
+    if (pathname === '/' || pathname === '/login' || pathname === '/register') {//if not logged in and on landing, login, register, don't show login button
+      return (null)
     }
-    if(this.props.auth.isAuthed) {
-      return(
+    if (this.props.auth.isAuthed) {
+      return (
         <div>
-            <LinkContainer to='/home'><Button id = "navButton" variant = "primary">Home</Button></LinkContainer>
-            <LinkContainer to='/profile'><Button id = "navButton" variant = "primary">My Profile</Button></LinkContainer>
+          <LinkContainer to='/home'><Button id="navButton" variant="primary">Home</Button></LinkContainer>
+          {(this.state.profile.profileType === "candidate") ?
+          <LinkContainer to='/applicantForm'><Button id="navButton" variant="primary">My Profile</Button></LinkContainer>
+          : <LinkContainer to='/applicantForm'><Button id="navButton" variant="primary">My Profile</Button></LinkContainer>}
+
         </div>
       )
     }
-    
+
   }
 
   search = () => {
-    const pathname=window.location.pathname
-    if(pathname==='/' || pathname ==='/login' || pathname === '/register'){//if not logged in and on landing, login, register, don't show login button
-      return(null)
+    const pathname = window.location.pathname
+    if (pathname === '/' || pathname === '/login' || pathname === '/register') {//if not logged in and on landing, login, register, don't show login button
+      return (null)
     }
-    if(this.props.auth.isAuthed) {
-      return(
+    if (this.props.auth.isAuthed) {
+      return (
         <div>
-            <LinkContainer to='/search'><Button id = "navButton" variant = "light">Search Employers</Button></LinkContainer>
+          <LinkContainer to='/search'><Button id="navButton" variant="light">Search Employers</Button></LinkContainer>
         </div>
       )
     }
-    
+
   }
 
   profileUtilities = () => {
-    const pathname=window.location.pathname
-    if(pathname==='/' || pathname ==='/login' || pathname === '/register'){//if not logged in and on landing, login, register, don't show login button
-      return(null)
+    const pathname = window.location.pathname
+    if (pathname === '/' || pathname === '/login' || pathname === '/register') {//if not logged in and on landing, login, register, don't show login button
+      return (null)
     }
-    if(this.props.auth.isAuthed) {
-      return(
+    if (this.props.auth.isAuthed) {
+      return (
         <div>
-            <LinkContainer to='/messages'><Button id = "navButton" variant ="light">Messages</Button></LinkContainer>
-            <LinkContainer to='/alerts'><Button id = "navButton" variant ="light">Alerts</Button></LinkContainer>
-            <LinkContainer to='/help'><Button id = "navButton" variant ="light">Help</Button></LinkContainer>        
+          <LinkContainer to='/messages'><Button id="navButton" variant="light">Messages</Button></LinkContainer>
+          <LinkContainer to='/alerts'><Button id="navButton" variant="light">Alerts</Button></LinkContainer>
+          <LinkContainer to='/help'><Button id="navButton" variant="light">Help</Button></LinkContainer>
         </div>
       )
     }
-    
+
     // if(pathname==='/' || pathname ==='/login' || pathname === '/register'){//if not logged in and on landing, login, register, don't show login button
     //   return(null)
     // }
@@ -80,22 +94,22 @@ class Navbar extends Component {
 
   render() {
     return (
-      <BSNav className = "navbar row">
+      <BSNav className="navbar row">
         <LinkContainer to="/">
           <BSNav.Brand>
             <img src={logo} height="125px" alt="logo" /> {/** emploYEET logo in navbar */}
           </BSNav.Brand>
         </LinkContainer>
-        
-        <Nav className = "col">
-          <this.utilities/>
+
+        <Nav className="col">
+          <this.utilities />
         </Nav>
-        <Nav className = "col">
-          <this.search/>
+        <Nav className="col">
+          <this.search />
         </Nav>
         <Nav>
-          <this.profileUtilities/>
-          <this.loginButton/>
+          <this.profileUtilities />
+          <this.loginButton />
         </Nav>
       </BSNav>
     )
