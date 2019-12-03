800ccc789d1864ec81a4274e366f096d00b677e9
tiffany-lam <tiffclam@gmail.com>
2019-12-02 21:49:00 -0800

    testing

--- client/src/components/_auth/Register.js
+++ client/src/components/_auth/Register.js
@@ -104,11 +104,9 @@ class Register extends Component {
           </Container>
         </Modal.Body>
         <Modal.Footer>
-<<<<<<< HEAD
-          <Button className="btn btn-primary" onClick={this._closeModal}>Close</Button>
-=======
+
           <Button onClick={this._closeModal}>Close</Button>
->>>>>>> parent of 0e17d2c... 🌊 BEAUTIFUL BUTTONS
+
         </Modal.Footer>
       </Modal>
     );
