6c7e5745a467656a9d09c334e04c59b56b380755
tiffany-lam <tiffclam@gmail.com>
2019-12-02 21:55:58 -0800

    Update Register.js

--- client/src/components/_auth/Register.js
+++ client/src/components/_auth/Register.js
@@ -104,9 +104,7 @@ class Register extends Component {
           </Container>
         </Modal.Body>
         <Modal.Footer>
-
           <Button onClick={this._closeModal}>Close</Button>
-
         </Modal.Footer>
       </Modal>
     );
